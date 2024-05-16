c9f97fbaabeb32cce042c52b6f464a0eacf4795b
kaikaci12 <somewonesomewone@gmail.com>
2024-05-16 21:21:29 +0400

    add heading info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import GoBack from "./components/GoBack";
+import AddInvoce from "./pages/AddInvoce";
 function App() {
   return (
     <>
       <Header />
-      <GoBack />
+      <div className="px-[24px] mt-[33px]">
+        <GoBack />
+        <AddInvoce />
+      </div>
     </>
   );
 }
--- src/pages/AddInvoce.jsx
+++ src/pages/AddInvoce.jsx
@@ -1,5 +1,13 @@
+import { space } from "postcss/lib/list";
 import React from "react";
 
 export default function AddInvoce() {
-  return <div></div>;
+  return (
+    <div className="">
+      <h2 className="text-[24px] font-bold leading-[32px] inline">Edit </h2>
+      <span className="text-[#888EB0] text-[24px] font-bold leading-[32px] tracking-[-0.5px]">
+        #<span className="text-black inline-flex">{"code"}</span>
+      </span>
+    </div>
+  );
 }
