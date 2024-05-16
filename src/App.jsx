afa08341a79a82906aa26981ee105231f8a2252a
kaikaci12 <somewonesomewone@gmail.com>
2024-05-16 22:51:14 +0400

    add other inputs and create separate components for some inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import GoBack from "./components/GoBack";
-import AddInvoce from "./pages/AddInvoce";
+import AddInvoce from "./pages/AddInvoice";
 function App() {
   return (
     <>
---
+++ src/components/CityInput.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function CityInput() {
+  return (
+    <>
+      <label
+        htmlFor="input"
+        className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   w-[45%]"
+      >
+        City
+        <input
+          type="text"
+          className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
+        />
+      </label>
+    </>
+  );
+}
