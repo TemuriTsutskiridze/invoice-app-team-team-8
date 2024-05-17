fac1b1d01d6cf7c9ac8f8722130ff1cda558c656
kaikaci12 <somewonesomewone@gmail.com>
2024-05-17 14:30:22 +0400

    add Invoice date

--- src/pages/AddInvoice.jsx
+++ src/pages/AddInvoice.jsx
@@ -1,6 +1,10 @@
 import { space } from "postcss/lib/list";
 import React from "react";
 import CountryInput from "../components/CountryInput";
+import PostCodeInput from "../components/PostCodeInput";
+import CityInput from "../components/CityInput";
+import StreetAdress from "../components/StreetAdress";
+import Calendar from "/assets/icon-calendar.svg";
 export default function AddInvoce() {
   return (
     <div className="">
@@ -17,39 +21,10 @@ export default function AddInvoce() {
         </span>
       </div>
       <form className="mt-[24px] flex flex-col gap-[45px]">
-        <div className="flex flex-col">
-          <label
-            htmlFor="input"
-            className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   "
-          >
-            Street Address
-            <input
-              type="text"
-              className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
-            />
-          </label>
-        </div>
+        <StreetAdress />
         <div className="flex  justify-between">
-          <label
-            htmlFor="input"
-            className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   w-[45%]"
-          >
-            City
-            <input
-              type="text"
-              className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
-            />
-          </label>
-          <label
-            htmlFor="input"
-            className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   w-[45%]"
-          >
-            Post Code
-            <input
-              type="text"
-              className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
-            />
-          </label>
+          <CityInput />
+          <PostCodeInput />
         </div>
         <CountryInput />
       </form>
@@ -77,39 +52,27 @@ export default function AddInvoce() {
             className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
           />
         </label>
-        <label
-          htmlFor="input"
-          className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   "
-        >
-          Street Address
-          <input
-            type="text"
-            className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
-          />
-        </label>
-        <div className="flex  justify-between">
-          <label
-            htmlFor="input"
-            className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   w-[45%]"
-          >
-            City
-            <input
-              type="text"
-              className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
-            />
-          </label>
-          <label
-            htmlFor="input"
-            className="text-[13px] font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] h-[48px]   w-[45%]"
-          >
-            Post Code
-            <input
-              type="text"
-              className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold leading-[15px] tracking-[-0.25px] text-black"
-            />
-          </label>
+
+        <div className="flex justify-between">
+          <CityInput />
+          <PostCodeInput />
         </div>
         <CountryInput />
+      </form>
+      <form className="flex flex-col gap-[45px] mt-[65px]">
+        <label
+          htmlFor="date"
+          className="  text-[15px]  leading-[15px]  font-medium tracking-[-0.1px] flex flex-col gap-[9px] text-[#7E88C3] "
+        >
+          <div className="absolute h-full w-full flex items-center justify-end">
+            <img src={Calendar} alt="calendar logo" />
+          </div>
+          Invoice Date
+          <input
+            type="date"
+            className=" border-[var(--05,#DFE3FA)] rounded-[4px] border-[1px] pr-[0] py-[15px] pl-[20px] text-[15px] font-bold h-[48px] leading-[15px] tracking-[-0.25px] text-black px-[16px]"
+          />
+        </label>
       </form>
     </div>
   );
