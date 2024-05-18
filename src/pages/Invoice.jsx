374128feb7854360dc366ec266e4c36f0ab1660f
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-05-18 12:55:44 -0700

    Fetch invoice data Based on provided id paramets made it intreactive

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Arrowleft from "/assets/icon-arrow-left.svg";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import data from "../data.json";
 
 function Invoice() {
+  const params = useParams();
+
   const [isDesktop, setIsDesktop] = useState(window.innerWidth > 640);
 
+  const invoiceById = data.find((invoice) => {
+    return invoice.id === params.id;
+  });
+  console.log(invoiceById);
   useEffect(() => {
     const handleResize = () => {
       setIsDesktop(window.innerWidth > 640);
@@ -38,15 +47,17 @@ function Invoice() {
       <div className="w-full h-[72px] bg-black"></div>
       <div className="Container flex justify-center flex-col px-[24px] py-0 w-full lg:px-[320px] xl:px-[450px]">
         <div className="dekstop 2xl: flex  flex-col justify-center items-center w-full">
-          <div className="Status flex justify-start align-start gap-[6px] pt-[33px] w-full mb-[33px]">
-            <img
-              src={Arrowleft}
-              alt=""
-              className="Arrow-left top w-[5px] h-[10px] m-[8px]"
-            />
-            <span className=" cursor-pointer text-[15px] font-bold tracking-[-0.25px] hover:text-[#7E88C3] font-[League_Spartan] transition-all duration-300 ease-linear">
-              Go back
-            </span>
+          <div className="Status flex  justify-start align-start gap-[6px] pt-[33px] w-full mb-[33px]">
+            <Link to={"/"} className="flex items-center gap-2">
+              <img
+                src={Arrowleft}
+                alt=""
+                className="Arrow-left top w-[5px] h-[10px] m-[8px]"
+              />
+              <span className=" cursor-pointer text-[15px] font-bold tracking-[-0.25px] hover:text-[#7E88C3] font-[League_Spartan] transition-all duration-300 ease-linear">
+                Go back
+              </span>
+            </Link>
           </div>
           <div className="StatusCheck flex justify-between items-center w-full h-[91px] sm:justify-between gap-[20px] rounded-[8px] bg-[#FFF] [box-shadow:0px_10px_10px_-10px_rgba(72,_84,_159,_0.10)] ">
             <div className="div flex flex-row items-center gap-[20px] max-sm:w-full max-sm:justify-between ">
@@ -56,8 +67,8 @@ function Invoice() {
               <ul className="mr-[48px] list-none relative w-fit flex items-center justify-center w-[104px] h-[40px] px-[10px]">
                 <div className="div absolute rounded-[6px] opacity-[0.0571] bg-[#FF8F00] top-0 left-0   flex-shrink-0  h-full w-full      z-[10]"></div>
                 <li className="text-[#FF8F00] font-[League_Spartan] text-[15px] font-bold leading-[15px] tracking-[-0.25px] relative">
-                  <span className="before:content-['\2022'] before:text-[#FF8F00] before:mr-[8px]">
-                    Pending
+                  <span className=" capitalize before:content-['\2022'] before:text-[#FF8F00] before:mr-[8px]">
+                    {invoiceById.status}
                   </span>
                 </li>
               </ul>
@@ -81,22 +92,22 @@ function Invoice() {
           <div className="Payment-Infoo flex justify-start items-center pt-[55px] w-full rounded-[8px] bg-[#FFF] [box-shadow:0px_10px_10px_-10px_rgba(72,_84,_159,_0.10)]">
             <div className="Detals flex justify-center items-center  w-full p-[24px] mb-[60px]  rounded-[8px] bg-[#FFF] [box-shadow:0px_10px_10px_-10px_rgba(72,_84,_159,_0.10)] l:w-full">
               <div className="Location flex flex-col w-full rounded-[8px] bg-[#FFF] [box-shadow:0px_10px_10px_-10px_rgba(72,_84,_159,_0.10)]">
-                <div className="div flex justify-between items-center w-full  max-sm:flex-col max-sm:items-start">
-                  <h2 className="font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px] tracking-[-0.25px] mb-[30px]">
-                    #XM9141
+                <div className="div flex justify-between items-center w-full gap  max-sm:flex-col max-sm:items-start">
+                  <h2 className="font-[League_Spartan] text-[15px] not-italic font-bold  pb-[3px] leading-[15px] tracking-[-0.25px] mb-[30px] ">
+                    #{invoiceById.id}
                     <span className="text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px] gap-[4px] flex flex-col">
-                      Graphic Design
+                      {invoiceById.description}
                     </span>
                   </h2>
                   <div className="ewe  text-right   flex flex-col w-fit  max-sm:text-left">
                     <span className="max-w-[127px] flex flex-col text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[18px]">
-                      84 Church Way
+                      {invoiceById.senderAddress.street}
                       <br />
-                      Bradford
+                      {invoiceById.senderAddress.city}
                       <br />
-                      BD1 9PB
+                      {invoiceById.senderAddress.postCode}
                       <br />
-                      United Kingdom
+                      {invoiceById.senderAddress.country}
                     </span>
                   </div>
                 </div>
@@ -104,20 +115,20 @@ function Invoice() {
                   <h2 className=" w-full text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px] gap-[4px]">
                     Invoice Date
                     <span className=" mt-[13px] text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[20px] tracking-[-0.25px] flex flex-col">
-                      21 Aug 2021
+                      {invoiceById.createdAt}
                     </span>
                   </h2>
                   <h2 className=" w-full text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px]">
                     Bill To
                     <span className="mt-[13px] text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[20px] tracking-[-0.25px] flex flex-col">
-                      Alex Grim
+                      {invoiceById.clientName}
                     </span>
                   </h2>
                   <div className="sent  w-full hidden sm:flex  justify-center items-center ">
                     <h2 className=" w-full flex flex-col gap-[13px] text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px]">
                       Sent to
                       <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[20px]">
-                        alexgrim@mail.com
+                        {invoiceById.clientEmail}
                       </span>
                     </h2>
                   </div>
@@ -126,18 +137,18 @@ function Invoice() {
                   <h2 className="mt-[25px] w-full flex flex-col gap-[13px] text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px]">
                     Payment Due
                     <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[20px]">
-                      20 Sep 2021
+                      {invoiceById.paymentDue}
                     </span>
                   </h2>
                   <div className="ewe  text-left   flex flex-col w-full ">
                     <span className="max-w-[127px] flex flex-col text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[18px]">
-                      84 Church Way
+                      {invoiceById.clientAddress.street}
                       <br />
-                      Bradford
+                      {invoiceById.clientAddress.city}
                       <br />
-                      BD1 9PB
+                      {invoiceById.clientAddress.postCode}
                       <br />
-                      United Kingdom
+                      {invoiceById.clientAddress.country}
                     </span>
                   </div>
                   <div className="div w-full max-sm:hidden"></div>
@@ -146,44 +157,35 @@ function Invoice() {
                   <h2 className="mt-[25px] flex flex-col gap-[13px] text-[#7E88C3] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px]">
                     Sent to
                     <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[20px]">
-                      alexgrim@mail.com
+                      {invoiceById.clientEmail}
                     </span>
                   </h2>
                 </div>
                 <div className="BIllmobile mt-[50px] ">
                   <div className="BiLL pt-[24px] pr-0 pb-0 pl-[24px]   flex flex-col sm:hidden">
-                    <div className="TOTAL mr-[32px]">
-                      <div className="bannerdesign flex flex-col">
-                        <div className="flex justify-between items-center relative">
-                          <h2 className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                            Banner Design
-                          </h2>
-                          <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px] absolute top-[23px] transform -translate-y-1/2 right-0">
-                            £ 156.00
-                          </span>
+                    {invoiceById.items.map((item, index) => {
+                      return (
+                        <div className="TOTAL mr-[32px]" key={index}>
+                          <div className="bannerdesign flex flex-col">
+                            <div className="flex justify-between items-center relative">
+                              <h2 className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+                                {item.name}
+                              </h2>
+                            </div>
+                            <div
+                              className="mt-[8px] mb-[23px] flex  items-center justify-between w-full
+                            ">
+                              <span className="text-[#7E88C3] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+                                {item.quantity} x £{item.price}
+                              </span>
+                              <span className="text-[#0C0E16] text-right font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+                                £ {item.total}
+                              </span>
+                            </div>
+                          </div>
                         </div>
-                        <div className="mt-[8px] mb-[23px]">
-                          <span className="text-[#7E88C3] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                            1 x £ 156.00
-                          </span>
-                        </div>
-                      </div>
-                      <div className="emaildesign flex flex-col mb-[23px]">
-                        <div className="flex justify-between items-center relative">
-                          <h2 className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                            Email Design
-                          </h2>
-                          <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px] absolute top-[23px] transform -translate-y-1/2 right-0">
-                            £ 400.00
-                          </span>
-                        </div>
-                        <div className="mt-[8px]">
-                          <span className="text-[#7E88C3] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                            2 x £ 200.00
-                          </span>
-                        </div>
-                      </div>
-                    </div>
+                      );
+                    })}
                   </div>
                 </div>
                 <div className="BiLL rounded-tl-[8px] rounded-br-none rounded-tr-[8px] rounded-bl-none bg-[#F9FAFE] py-[20px] mt-[50px] px-[45px]">
@@ -195,40 +197,30 @@ function Invoice() {
                         <span>Price</span>
                         <span>Price</span>
                       </div>
-                      <div className="div w-full flex justify-between"></div>
-                      <div className="flex justify-between items-center relative w-full  mb-[32px]">
-                        <h2 className="text-[#0C0E16] font-[League_Spartan] text-[15px] font-bold leading-[15px]">
-                          Banner Design
-                        </h2>
-                        <span className="text-[#7E88C3] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                          1
-                        </span>
-                        <span className="text-[#7E88C3] text-right font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                          £ 156.00
-                        </span>
-                        <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] font-bold leading-[15px]">
-                          {" "}
-                          £ 156.00
-                        </span>
-                      </div>
                     </div>
-                    <div className="emaildesign flex flex-col mb-[23px]">
-                      <div className="flex justify-between items-center relative">
-                        <h2 className="text-[#0C0E16] font-[League_Spartan] text-[15px] font-bold leading-[15px]">
-                          Banner Design
-                        </h2>
-                        <span className="text-[#7E88C3] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                          2
-                        </span>
-                        <span className="text-[#7E88C3] text-right font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
-                          £ 200.00
-                        </span>
-                        <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] font-bold leading-[15px]">
-                          {" "}
-                          £ 400.00
-                        </span>
-                      </div>
-                    </div>
+                    {invoiceById.items.map((item, index) => {
+                      return (
+                        <div
+                          className="div w-full flex justify-between"
+                          key={index}>
+                          <div className="flex justify-between items-center relative w-full  mb-[32px]">
+                            <h2 className="text-[#0C0E16] font-[League_Spartan] text-[15px] font-bold leading-[15px]">
+                              {item.name}
+                            </h2>
+                            <span className="text-[#7E88C3] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+                              {item.quantity}
+                            </span>
+                            <span className="text-[#7E88C3] text-right font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+                              £ {item.price}
+                            </span>
+                            <span className="text-[#0C0E16] font-[League_Spartan] text-[15px] font-bold leading-[15px]">
+                              {" "}
+                              £ {item.total}
+                            </span>
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
                 <div className="bomboclat flex flex-row justify-between rounded-tl-none rounded-br-[8px] rounded-tr-none rounded-bl-[8px] bg-[#373B53] h-[80px] mt-5 p-4 lg:mt-0">
@@ -239,7 +231,7 @@ function Invoice() {
                     Grand Total
                   </span>
                   <h2 className="mt-[2px] text-[#FFF] text-right font-[League_Spartan] text-[24px] not-italic font-bold leading-[32px]">
-                    £ 556.00
+                    £ {invoiceById.total}
                   </h2>
                 </div>
               </div>
--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -3,10 +3,59 @@ import Card from "../Components/Card";
 export default function Invoices() {
   console.log(data);
   return (
-    <div className="div flex w-full justify-center items-center flex-wrap gap-[5px]">
-      {data.map((id, idx) => {
-        return <Card key={idx} invoice={id} />;
-      })}
+    <div className="flex justify-center items-center mt-[100px]">
+      <div className="container flex flex-col justify-center items-center w-[730px]">
+        <div className="dvi flex flex-row justify-between items-center w-full mb-[50px]  ">
+          <div className="total flex flex-row justify-center items-center ">
+            <h2 className="text-[#0C0E16] font-[League_Spartan] text-[36px] not-italic font-bold leading-[normal] tracking-[-1.125px] flex flex-col">
+              Invoices{" "}
+              <span className="text-[#888EB0] font-[League_Spartan] text-[13px] not-italic font-medium leading-[15px]">
+                There are 7 total invoices
+              </span>
+            </h2>
+          </div>
+          <div className="filter flex flex-row justify-center items-center gap-5 ">
+            <div className="filter ">
+              <h2 className="flex flex-row items-center justify-center gap-[14px] text-[#0C0E16] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+                Filter by status
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="7"
+                  viewBox="0 0 10 7"
+                  fill="none">
+                  <path
+                    d="M1 1L5.2279 5.2279L9.4558 1"
+                    stroke="#7C5DFA"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </h2>
+            </div>
+
+            <button className="flex flex-row justify-center items-center w-[150px] h-[48px] rounded-[24px] gap-3 bg-[#7C5DFA] text-[#FFF] font-[League_Spartan] text-[15px] not-italic font-bold leading-[15px]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none">
+                <circle cx="16" cy="16" r="16" fill="white" />
+                <path
+                  d="M17.3131 21.0229V17.3131H21.0229V14.7328H17.3131V11.0229H14.7328V14.7328H11.0229V17.3131H14.7328V21.0229H17.3131Z"
+                  fill="#7C5DFA"
+                />
+              </svg>
+              New Invoice
+            </button>
+          </div>
+        </div>
+        <div className="div flex w-full justify-center items-center flex-wrap gap-[5px]">
+          {data.map((id, idx) => {
+            return <Card key={idx} invoice={id} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
